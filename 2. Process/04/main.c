5738a7e26dba627252f8d433f020f3e341098b6e
ITSC_Mac <itsc_macbook@ITSC-Macui-MacBookPro-2.local>
2021-04-29 19:47:29 +0900

    Process

---
+++ 2. Process/04/main.c
@@ -0,0 +1,41 @@
+/**
+ * Keep Running Your Process.
+ *
+ * By walking through this example you’ll learn:
+ * - How to use execl() while keep your process using fork().
+ * 
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main(int argc, char* argv[]){
+    pid_t pid = fork();
+
+    switch (pid)
+    {
+        default:
+            // HINT: The parent process should fall into this scope.
+            printf("I'm your father.\n");
+            sleep(3);
+            break;
+
+        case 0:
+            sleep(1);
+            // HINT: The child process should fall into this scope.
+            printf("I'm sorry, but I'm not Luke. I'm...");
+            fflush(stdout);
+
+            sleep(1); // for dramatic effect
+
+            // HINT: The /usr/bin/whoami should be executed.
+            execl("/usr/bin/whoami", "/usr/bin/whoami", (char*)NULL);
+
+
+        case -1:
+            printf("WTF?");
+            return -1;
+            break;
+    }
+}
