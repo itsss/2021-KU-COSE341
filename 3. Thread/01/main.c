8dfc65f6cb1a70f8daeb12b29fbc4e392da0413c
ITSC_Mac <itsc_macbook@ITSC-Macui-MacBookPro-2.local>
2021-05-08 03:13:05 +0900

    thread

---
+++ 3. Thread/01/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+   #include <unistd.h>
+   #include <pthread.h>
+   #include <sys/wait.h>
+  
+   void* ninja(void* arg){
+   printf("Who’s there?");
+   fflush(stdout);
+  
+   pthread_exit("ninja");
+   }
+  
+  
+  
+   //
+   // Expected output:
+   //
+   // Knock knock.
+   // Who's there? - from ninja
+   // Knuc...kles.
+   //
+  
+   int main(int argc, char* argv[]){
+   pthread_t tid;
+   char* from = "";
+  
+   printf("Knock knock.\n");
+  
+   // HINT: The thread that runs `ninja` should be created.
+   int status = pthread_create(&tid, NULL, &ninja, NULL);
+  
+   if(status != 0){
+   printf("WTF?");
+   return -1;
+   }
+  
+   // HINT: The main thread should not be exited until `ninja` has finished.
+   pthread_join(tid, (void**)&from);
+  
+   // HINT: The variable `from` should not be empty.
+   printf(" - from %s\n", from);
+  
+   printf("Knuc...kles.\n");
+  
+   return 0;
+   }
+
+
